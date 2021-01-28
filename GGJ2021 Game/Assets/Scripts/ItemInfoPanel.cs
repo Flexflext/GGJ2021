ad7e8bc132b840da53af9685ba83d2cfd4d643a5
AEmmerich98 <AEmmerich98@users.noreply.github.com>
2021-01-28 18:52:45 +0100

    Improved Item System

---
+++ GGJ2021 Game/Assets/Scripts/ItemInfoPanel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemInfoPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PanelObject;
+    [SerializeField]
+    private TMP_Text NameText;
+    [SerializeField]
+    private TMP_Text DescriptionText;
+
+    private Item DisplayedItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseInfo();
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit && DisplayedItem == null)
+        {
+            DisplayInfo(hit.transform.gameObject.GetComponent<Item>());
+        }
+        else if (!hit && PanelObject.activeSelf == true)
+        {
+            CloseInfo();
+        }
+    }
+
+    
+
+    private void CloseInfo()
+    {
+        PanelObject.SetActive(false);
+        DisplayedItem = null;
+    }
+
+    public void DisplayInfo(Item _item)
+    {
+        DisplayedItem = _item;
+
+        PanelObject.SetActive(true);
+
+        NameText.text = _item.Name;
+        NameText.color = _item.Rarity.NameColor;
+
+        DescriptionText.text = _item.GetItemInfo();
+
+        this.transform.position = _item.transform.position;
+    }
+}
