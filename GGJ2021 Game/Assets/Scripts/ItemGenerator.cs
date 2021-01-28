d1179db68b7a63bc76a144cdf0f3a2d9b5357d68
Leo Hilbert <lhilbert@live.de>
2021-01-28 14:29:10 +0100

    added basic item spawning

--- GGJ2021 Game/Assets/Scripts/ItemGenerator.cs
+++ GGJ2021 Game/Assets/Scripts/ItemGenerator.cs
@@ -1,30 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class ItemGenerator : MonoBehaviour
 {
-    public enum ERarity
+    private enum ERarity
     {
         Common,
         Uncommon,
         Rare,
         Epic,
-        Legandary
+        Legendary
     }
 
-    [SerializeField]
-    private ItemRarity[] ItemRarities;
+    [SerializeField] private Sprite itemSprite;
+    [SerializeField] private ItemRarity[] itemRarities;
 
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> GenerateItems(int amount)
     {
-        
+        List<GameObject> items = new List<GameObject>(amount);
+        for (int i = 0; i < amount; i++)
+        {
+            var item = new GameObject();
+            var itemBehaviour = GenerateBehaviour(item);
+            itemBehaviour.Name = NameGenerator.generateName();
+            itemBehaviour.Rarity = itemRarities[(int) GenerateRarity()];
+            item.name = itemBehaviour.Name;
+            itemBehaviour.Icon = itemSprite;
+            
+            var spriteRenderer = item.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = itemSprite;
+            spriteRenderer.color = itemBehaviour.Rarity.NameColor;
+            items.Add(item);
+        }
+
+        return items;
     }
 
-    // Update is called once per frame
-    void Update()
+    private static Item GenerateBehaviour(GameObject item)
     {
-        
+        //TODO more item types
+        return item.AddComponent<UnusableItem>();
+    }
+
+    private static ERarity GenerateRarity()
+    {
+        var rarity = Random.Range(0, 100);
+        if (rarity >= 99)
+        {
+            return ERarity.Legendary;
+        }
+
+        if (rarity >= 97)
+        {
+            return ERarity.Epic;
+        }
+
+        if (rarity >= 94)
+        {
+            return ERarity.Rare;
+        }
+
+        if (rarity >= 70)
+        {
+            return ERarity.Uncommon;
+        }
+
+        return ERarity.Common;
     }
 }
---
+++ GGJ2021 Game/Assets/Scripts/NameGenerator/NameGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class NameGenerator
+{
+    private static List<string> Adjectives = readFile("Assets/Text/adjectives.txt");
+    private static List<string> Names = readFile("Assets/Text/names.txt");
+
+    private static List<string> readFile(string file)
+    {
+        var stream = new StreamReader(file);
+        var lines = new List<string>();
+        while (!stream.EndOfStream)
+            lines.Add(stream.ReadLine());
+        return lines;
+    }
+
+    public static string generateName()
+    {
+        var adjectiveIndex = Random.Range(0, Adjectives.Count);
+        var nameIndex = Random.Range(0, Names.Count);
+        return Adjectives[adjectiveIndex] + " " + Names[nameIndex];
+    }
+}
