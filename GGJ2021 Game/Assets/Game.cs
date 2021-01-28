6c7396f9c32b7d00608a4c52191d4188af425126
nikzeeKraken <nick.zohren@outlook.com>
2021-01-28 20:10:08 +0100

    Added Inventory UI
    Changed Sprite Settings
    Added Game manager and Uimanager
    referenzen zu den inventory spirtes im UI manager über Game.instance.UImanager.InventorySprites

---
+++ GGJ2021 Game/Assets/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game Instance;
+    [SerializeField]
+    private UIManager m_UIManager;
+    public UIManager UIManager
+    {
+        get { return m_UIManager; }
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+            Destroy(this);
+    }
+}
