ef9dfe06c52b670e0016b50b21e68ff4515f4fa0
Nico <n.nowak@sae.edu>
2021-01-28 17:10:05 +0100

    Updating from Main
    Updating from Main-Brunch so I am up to date

---
+++ GGJ2021 Game/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance; // AudioManager instance
+    public Sound[] sounds; // Array of Sounds
+
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+
+    }
+
+
+
+    /// <summary>
+    /// Plays Sound, Sound-ScriptableObject parameter
+    /// </summary>
+    /// <param name="_sound"></param>
+    public void PlaySound(Sound _sound)
+    {
+        Sound s = Array.Find(sounds, sound => sound == _sound); // finds the sound that should be played in the sounds array
+
+        if (s == null)
+        {
+            if (_sound == null)
+            {
+                Debug.Log("No Sound given");
+                return;
+            }
+            Debug.Log("Sound '" + _sound.name + "' not found");
+            return;
+        }
+        else if (s.source == null)
+        {
+            Debug.Log("No Sound source");
+            return;
+        }
+        s.source.Play();
+    }
+
+    /// <summary>
+    /// Plays Sound, string parameter
+    /// </summary>
+    /// <param name="name"></param>
+    public void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound '" + name + "' not found");
+            return;
+        }
+        else if (s.source == null)
+        {
+            Debug.Log("No Sound source");
+            return;
+        }
+        s.source.Play();
+    }
+
+}
