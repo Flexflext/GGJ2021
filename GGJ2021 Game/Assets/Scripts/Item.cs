db6a14a12cfa1f36573f42759f08b3aabca4cfc8
AEmmerich98 <AEmmerich98@users.noreply.github.com>
2021-01-28 12:35:24 +0100

    Added some scripts

---
+++ GGJ2021 Game/Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    
+
+    
+
+    public string Name;
+    public ItemRarity Rarity;
+    public Sprite Icon;
+
+    
+    //
+    void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void GenerateRandomStats();
+}
---
+++ GGJ2021 Game/Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public enum ERarity
+    {
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legandary
+    }
+
+    [SerializeField]
+    private ItemRarity[] ItemRarities;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
